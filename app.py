8708ce71f4345caa631110fd8c28bc2f90c363e0
condimoz <rodrigoborges82@gmail.com>
2026-06-18 18:41:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,14 @@ def baixar_audio_youtube(url):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'quiet': True
+        'quiet': True,
+        'nocheckcertificate': True,
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-us,en;q=0.5',
+            'Sec-Fetch-Mode': 'navigate',
+        }
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         str.info("Baixando e processando o áudio do YouTube...")
